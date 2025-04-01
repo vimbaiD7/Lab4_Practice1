6882878a18d925e47c1e95c63d4ad081d3e8ca24
vimbaiD7 <172248661+vimbaiD7@users.noreply.github.com>
2025-04-01 15:23:20 +0200

    PDF DONE

---
+++ FakeStore/FakeStoreApiClient/Product.cs
@@ -0,0 +1,23 @@
+﻿using System.Text.Json.Serialization;
+
+namespace FakeStoreApiClient;
+public class Product
+{
+    [JsonPropertyName("id")]
+    public int Id { get; set; }
+
+    [JsonPropertyName("title")]
+    public string Title { get; set; }
+
+    [JsonPropertyName("price")]
+    public float Price { get; set; }
+
+    [JsonPropertyName("description")]
+    public string Description { get; set; }
+
+    [JsonPropertyName("category")]
+    public string Category { get; set; }
+
+    [JsonPropertyName("image")]
+    public string Image { get; set; }
+}
--- FakeStore/FakeStoreApiClient/Program.cs
+++ FakeStore/FakeStoreApiClient/Program.cs
@@ -1,1 +1,13 @@
-﻿Console.WriteLine("Hello, Programming Languages 1!");
+﻿
+using System.Net.Http.Json;
+using FakeStoreApiClient;
+
+var httpClient = new HttpClient();
+httpClient.BaseAddress = new Uri("https://fakestoreapi.com/");
+
+var product = await httpClient.GetFromJsonAsync<Product>("products/1");
+
+product.Price = 1000;
+
+var response = await httpClient.PutAsJsonAsync("products/1", product);
+Console.WriteLine(await response.Content.ReadAsStringAsync());
